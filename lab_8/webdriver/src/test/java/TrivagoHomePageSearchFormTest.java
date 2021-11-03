5b7ade4d4a9bae93596cf8fa98d07ef9b45476f0
arsen-zaharenko <59254411+arsen-zaharenko@users.noreply.github.com>
2021-11-04 00:08:44 +0300

    Update TrivagoHomePageSearchFormTest.java

--- lab_8/webdriver/src/test/java/TrivagoHomePageSearchFormTest.java
+++ lab_8/webdriver/src/test/java/TrivagoHomePageSearchFormTest.java
@@ -1,6 +1,6 @@
 import org.openqa.selenium.*;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
