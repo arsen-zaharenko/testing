de2f4d8b0399f805eec35dfe19bf6eb673e6b740
arsen-zaharenko <59254411+arsen-zaharenko@users.noreply.github.com>
2021-12-06 00:10:23 +0300

    Update TrivagoCarsPage.java

--- lab_9/framework/src/main/java/page/TrivagoCarsPage.java
+++ lab_9/framework/src/main/java/page/TrivagoCarsPage.java
@@ -14,7 +14,7 @@ public class TrivagoCarsPage extends AbstractPage {
 
     private By currencyPickerButtonLocator = By.xpath("//div[contains(@class,'currency-picker')]");
 
-    private By locationDivLocator = By.xpath("//div[@class='NbWx-locationInput']");
+    private By locationDivLocator = By.xpath("//div[contains(@class,'locationInput')]");
 
     private By locationInputLocator = By.xpath("//input[@class='k_my-input']");
 
