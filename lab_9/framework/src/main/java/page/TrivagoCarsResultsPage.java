58225f6637d64b0738740f8f9998ad9ff682010e
arsen-zaharenko <59254411+arsen-zaharenko@users.noreply.github.com>
2021-12-06 19:06:59 +0300

    Update TrivagoCarsResultsPage.java

--- lab_9/framework/src/main/java/page/TrivagoCarsResultsPage.java
+++ lab_9/framework/src/main/java/page/TrivagoCarsResultsPage.java
@@ -17,15 +17,11 @@ public class TrivagoCarsResultsPage extends AbstractPage {
     }
 
     public boolean isInitialized(String location, String currency) {
-        By currencyLocator = By.xpath("//div[@class='EuxN-Current']");
-        By locationLocator = By.xpath("//div[@class='NbWx-locationInput']/div/div/div[2]");
-        
-        new WebDriverWait(driver, WAIT_TIMEOUT_SECONDS)
-                .until(ExpectedConditions
-                        .visibilityOf(findElementByLocator(locationLocator)));
-        
         LOGGER.log(Level.INFO, driver.getCurrentUrl());
         LOGGER.log(Level.INFO, driver.getPageSource());
+        
+        By currencyLocator = By.xpath("//div[@class='EuxN-Current']");
+        By locationLocator = By.xpath("//div[@class='NbWx-locationInput']/div/div/div[2]");
         
         if (findElementByLocatorAndGetText(locationLocator).contains(location)
             && findElementByLocatorAndGetText(currencyLocator).contains(currency)) {
