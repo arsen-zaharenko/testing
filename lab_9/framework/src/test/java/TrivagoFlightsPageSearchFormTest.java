604b9d25b360ecb7a797ca3c6c1ac78207e81d44
arsen-zaharenko <59254411+arsen-zaharenko@users.noreply.github.com>
2021-12-07 01:28:26 +0300

    Update TrivagoFlightsPageSearchFormTest.java

--- lab_9/framework/src/test/java/TrivagoFlightsPageSearchFormTest.java
+++ lab_9/framework/src/test/java/TrivagoFlightsPageSearchFormTest.java
@@ -5,28 +5,45 @@ import page.TrivagoHomePage;
 import util.CommonConditions;
 
 public class TrivagoFlightsPageSearchFormTest extends CommonConditions {
+    private static final String LOCATION_FROM = "";
+    private static final String LOCATION_TO = "";
+    private static final String TRIP_TYPE = "Multi-city";
+    private static final int MAX_NUMBER_OF_FORMS = 6;
+
     @Test
     public void emptyFromAndToFieldsFlightsPageTest() {
         TrivagoHomePage homePage = new TrivagoHomePage(driver);
+        TrivagoFlightsPage flightsPage = homePage.openFlightsPage();
 
-        TrivagoFlightsPage flightsPage = homePage.openFlightsPage()
-                                                 .findFlights();
+        flightsPage.searchFlights();
 
         Assert.assertTrue(flightsPage.isLocationExceptionVisible());
     }
 
     @Test
     public void sameFromAndToFieldsFlightsPageTest() {
+        TrivagoHomePage homePage = new TrivagoHomePage(driver);
+        TrivagoFlightsPage flightsPage = homePage.openFlightsPage();
 
-    }
+        final String[] locations = flightsPage.enterFromLocation(LOCATION_FROM)
+                                              .enterToLocation(LOCATION_TO)
+                                              .getFromAndToLocations();
 
-    @Test
-    public void swapFromAndToFieldsFlightsPageTest() {
+        final String[] changedLocations = flightsPage.swapLocations()
+                                                     .getFromAndToLocations();
 
+        Assert.assertTrue(locations[0].equals(changedLocations[1]) && locations[1].equals(changedLocations[0]));
     }
 
     @Test
     public void maxNumberOfFlightFormsFlightsPageTest() {
+        TrivagoHomePage homePage = new TrivagoHomePage(driver);
+        TrivagoFlightsPage flightsPage = homePage.openFlightsPage();
 
+        final int maxNumberOfForms = flightsPage.changeTripType(TRIP_TYPE)
+                                                .addMaxNumberOfFlightForm()
+                                                .getNumberOfFlightForms();
+
+        Assert.assertEquals(MAX_NUMBER_OF_FORMS, maxNumberOfForms);
     }
 }
