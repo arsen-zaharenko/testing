a36b26a15a7cc00a5360167b5f86239b0156bec1
arsen-zaharenko <59254411+arsen-zaharenko@users.noreply.github.com>
2021-12-07 18:54:19 +0300

    Update TrivagoCarsPageSearchFormTest.java

--- lab_9/framework/src/test/java/TrivagoCarsPageSearchFormTest.java
+++ lab_9/framework/src/test/java/TrivagoCarsPageSearchFormTest.java
@@ -9,7 +9,7 @@ public class TrivagoCarsPageSearchFormTest extends CommonConditions {
     private static final String CURRENCY = "Br";
     private static final String LOCATION = "Minsk";
     private static final String LOCATION_EXCEPTION_TEXT = "Please pick a pick-up location.";
-/*
+
     @Test
     public void emptyFromFieldCarsPageTest() {
         TrivagoHomePage homePage = new TrivagoHomePage(driver);
@@ -32,5 +32,4 @@ public class TrivagoCarsPageSearchFormTest extends CommonConditions {
 
         Assert.assertTrue(resultsPage.isInitialized());
     }
-    */
 }
